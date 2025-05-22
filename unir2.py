f5820f5824873ebac606eed7998543e04e568cd1
romanstaff04 <romandariostaffolani@gmail.com>
2025-05-22 11:56:20 -0300

    logica finalizada

--- unir2.py
+++ unir2.py
@@ -1,31 +1,15 @@
 import pandas as pd
 import glob
 import os
-from openpyxl import load_workbook
-from openpyxl.styles import PatternFill
 
-"""def marcarDuplicadosEnExcel(nombre_archivo, duplicados):
-    wb = load_workbook(nombre_archivo)
-    ws = wb.active
-    rojo = PatternFill(start_color="FFFF0000", end_color="FFFF0000", fill_type="solid")
-
-    for i, es_duplicado in enumerate(duplicados, start=2):
-        if es_duplicado:
-            ws[f"Y{i}"].fill = rojo
-    wb.save(nombre_archivo)"""
-
-def borrarORG(df, iata):
-    if iata == "crd".upper():
+def orgCourier(df, iata, lista1):
+    if iata in lista1:
             df.loc[df["Nombre Solicitante"] == "ORG COURIER ARG", "Ruta Virtual"] = 700
-    elif iata == "luq".upper():
-        df.loc[df["Nombre Solicitante"] == "ORG COURIER ARG", "Ruta Virtual"] = 700
-    elif iata == "irj".upper():
-        pass
     else:
         print("error")
     return df
 
-def borrarMHTML(): #si encuentra archivos mhtml preguntar si lo queres borrar
+def borrarMHTML():
     while True:
         pregunta = input("queres borrar los archivos MHTML?: S/N ").lower()
         if pregunta == "s":
@@ -55,14 +39,52 @@ def manipularDatos(df, iata):
 
     # agregar valores
     df.loc[df["Peso del objeto"] >= 200, "Ruta Virtual"] = 503
-    df.loc[df["Volumen"] >= 1.5, "Ruta Virtual"] = 503
+    df.loc[df["Volumen"] >= 0.7, "Ruta Virtual"] = 503
 
     #limpiar columnas
     df["Distrito Destino"] = ""
     df["Provincia"] = ""
 
-    duplicados = df.duplicated(subset= "Nro. identificación pieza según cliente", keep = False)
-    return df, duplicados
+    #duplicados = df.duplicated(subset= "Nro. identificación pieza según cliente", keep = False)
+    return df
+
+def canalizadorLocalidad(df, iata):
+    # --- MERGE con CANALIZADOR para que traiga la localidad ---
+        canalizadorLocalidad = pd.read_excel("CANALIZADOR MADRE.xlsx")
+        canalizador_reducidoLocalidad = canalizadorLocalidad[["CP Destino", "Distrito Destino"]]
+
+        #elimina la columna distrito destino original para luego reemplazarla por el merge-
+        df = df.drop(columns=["Distrito Destino"], errors="ignore")
+        merge = pd.merge(df, canalizador_reducidoLocalidad, on="CP Destino", how="left")
+
+        # Insertar "Distrito Destino" después de "Altura"
+        columna_referencia = "Altura"
+        if columna_referencia in merge.columns:
+            indice_destino = merge.columns.get_loc(columna_referencia) + 1
+            columna_valores = merge.pop("Distrito Destino")
+            merge.insert(indice_destino, "Distrito Destino", columna_valores)
+        else:
+            print(f"Advertencia: no se encontró la columna '{columna_referencia}' para ubicar 'Distrito Destino'. Se dejó al final.")
+        return merge
+
+def canalizadorProvincia(df, iata):
+    # --- MERGE con CANALIZADOR para que traiga la provincia ---
+        canalizadorProvincia = pd.read_excel("CANALIZADOR MADRE.xlsx")
+        canalizador_reducidoProvincia = canalizadorProvincia[["CP Destino", "Provincia"]]
+
+        #elimina la columna distrito destino original para luego reemplazarla por el merge-
+        df = df.drop(columns=["Provincia"], errors="ignore")
+        merge = pd.merge(df, canalizador_reducidoProvincia, on="CP Destino", how="left")
+
+        # Insertar "Provincia" después de "Poblacion"
+        columna_referencia = "Población"
+        if columna_referencia in merge.columns:
+            indice_destino = merge.columns.get_loc(columna_referencia) + 1
+            columna_valores = merge.pop("Provincia")
+            merge.insert(indice_destino, "Provincia", columna_valores)
+        else:
+            print(f"Advertencia: no se encontró la columna '{columna_referencia}' para ubicar 'Provincia'. Se dejó al final.")
+        return merge
 
 def main():
     while True:
@@ -70,65 +92,29 @@ def main():
         if len(iata) == 3:
             break
         else:
-            print("error intente nuevametne")
+            print("Error. Intente nuevamente.")
 
-    if iata == "crd".upper(): #listo
+    lista1 = ["CRD", "LUQ"]
+    if iata in lista1:
         encontrar = glob.glob("*xlsx")
         lista = []
-        for archivo in encontrar:
-            leer = pd.read_excel(archivo)
-            lista.append(leer)
-        
-        df = pd.concat(lista, ignore_index=True)
-        df, duplicados  = manipularDatos(df, iata)
-        df = borrarORG(df, iata)  
-        
-        df.to_excel(f"archivoUnificado{iata}.xlsx", index=False)
-        #marcarDuplicadosEnExcel(f"archivoUnificado{iata}.xlsx", duplicados)
-        borrarMHTML()
-        ejecutarExcelFinalizado(iata)
 
-    elif iata == "luq".upper(): # listo
-        encontrar = glob.glob("*xlsx")
-        lista = []
         for archivo in encontrar:
+            if archivo == "CANALIZADOR MADRE.xlsx":
+                continue  # saltea el canalizador
             leer = pd.read_excel(archivo)
             lista.append(leer)
-        
-        df = pd.concat(lista, ignore_index=True)
-        df, duplicados  = manipularDatos(df, iata)
-        df = borrarORG(df, iata)  
-        
-        df.to_excel(f"archivoUnificado{iata}.xlsx", index=False)
-        #marcarDuplicadosEnExcel(f"archivoUnificado{iata}.xlsx", duplicados)
-        borrarMHTML()
-        ejecutarExcelFinalizado(iata)
 
-    elif iata == "irj".upper():
-        encontrar = glob.glob("*xlsx")
-        lista = []
-        for archivo in encontrar:
-            leer = pd.read_excel(archivo)
-            lista.append(leer)
-        df = pd.concat(lista, ignore_index=True)
-        #solo vaciar columnas 
-        df["Distrito Destino"] = ""
-        df["Provincia"] = ""
-        df.to_excel(f"archivoUnificado{iata}.xlsx", index=False)
-        borrarMHTML()
-        ejecutarExcelFinalizado(iata)
-    
-    elif iata == "fma".upper():
-        encontrar = glob.glob("*xlsx")
-        lista = []
-        for archivo in encontrar:
-            leer = pd.read_excel(archivo)
-            lista.append(leer)
         df = pd.concat(lista, ignore_index=True)
         df = manipularDatos(df, iata)
-        
+        df = orgCourier(df, iata, lista1)
+        df = canalizadorLocalidad(df, iata)
+        df = canalizadorProvincia(df,iata)
 
-    else:
-        print("error")
+        # Guardar y abrir
+        nombre_salida = f"archivoUnificado{iata}.xlsx"
+        df.to_excel(nombre_salida, index=False)
+        os.startfile(nombre_salida)
+
 if __name__ == "__main__": 
     main()
